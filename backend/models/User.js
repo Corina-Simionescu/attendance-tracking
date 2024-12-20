35fb5a1f00073ff0efcd369ecfe12bd19dc6ce36
Corina717 <corina.simionescu12@gmail.com>
2024-12-20 17:17:43 +0200

    Add user model

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const sequelize = require("../config/database.js");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+  }
+);
+
+module.exports = User;
