bcb5bd0c984ba2ce7e1c3c86e333433003b6bc35
Corina717 <corina.simionescu12@gmail.com>
2024-12-21 16:53:09 +0200

    Add EventGroup model

---
+++ backend/models/EventGroup.js
@@ -0,0 +1,27 @@
+const { sequelize } = require("../config/database.js");
+const { DataTypes } = require("sequelize");
+
+const EventGroup = sequelize.define("EventGroup", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Users",
+      key: "id",
+    },
+  },
+});
+
+module.exports = EventGroup;
