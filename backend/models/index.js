3ca5148e30b3516a7fd8dd1969160654613c6c84
Corina717 <corina.simionescu12@gmail.com>
2024-12-21 16:54:59 +0200

    Add associations for models

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,10 +1,23 @@
 const User = require("./User.js");
+const EventGroup = require("./EventGroup.js");
 const Event = require("./Event.js");
+const Attendance = require("./Attendance.js");
 
-User.hasMany(Event);
-Event.belongsTo(User);
+User.hasMany(EventGroup, { foreignKey: "userId" });
+EventGroup.belongsTo(User, { foreignKey: "userId" });
+
+EventGroup.hasMany(Event, { foreignKey: "eventGroupId" });
+Event.belongsTo(EventGroup, { foreignKey: "eventGroupId" });
+
+User.hasMany(Event, { foreignKey: "userId" });
+Event.belongsTo(User, { foreignKey: "userId" });
+
+Event.hasMany(Attendance, { foreignKey: "eventId" });
+Attendance.belongsTo(Event, { foreignKey: "eventId" });
 
 module.exports = {
   User,
+  EventGroup,
   Event,
+  Attendance,
 };
