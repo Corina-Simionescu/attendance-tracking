8c2e0bd23b83da071b00664d8fcdaf5ad36e16de
Corina717 <corina.simionescu12@gmail.com>
2024-12-21 16:53:27 +0200

    Add Attendance model

---
+++ backend/models/Attendance.js
@@ -0,0 +1,43 @@
+const { sequelize } = require("../config/database.js");
+const { DataTypes } = require("sequelize");
+
+const Attendance = sequelize.define(
+  "Attendance",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    eventId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Events",
+        key: "id",
+      },
+    },
+    participantName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    participantEmail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    timestamp: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["eventId", "participantEmail"],
+      },
+    ],
+  }
+);
+
+module.exports = Attendance;
