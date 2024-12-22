cae7313343d4eaaa6a806db823b0f24052395e37
Corina717 <corina.simionescu12@gmail.com>
2024-12-22 21:01:27 +0200

    Add OpenAPI/Swagger documentation for auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
 const express = require("express");
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs")
+const YAML = require('yaml')
 
 const { initializeDatabase } = require("./config/database.js");
 const routes = require("./routes/index.js");
@@ -10,6 +12,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 initializeDatabase();
 
