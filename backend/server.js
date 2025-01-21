5c016f0be58b9f35e8c18136fcfeec443b82a827
Corina717 <corina.simionescu12@gmail.com>
2025-01-21 11:13:13 +0200

    Update API documentation
    - Add documentation for all existing endpoints
    - Include detailed request/response schemas for each route
    - Add component schemas for all models (User, Event, EventGroup, Attendance)

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const swaggerUi = require('swagger-ui-express');
-const fs = require("fs")
-const YAML = require('yaml')
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const YAML = require("yaml");
 
 const { initializeDatabase } = require("./config/database.js");
 const routes = require("./routes/index.js");
@@ -13,10 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const file  = fs.readFileSync('./swagger.yaml', 'utf8')
-const swaggerDocument = YAML.parse(file)
+const file = fs.readFileSync("./swagger.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 initializeDatabase();
 
